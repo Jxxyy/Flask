3fc54e1a06e5a3fd661b0fb41ecbafd7bd5211cd
Jxxyy <smtliuguiqian@163.com>
2020-06-21 19:04:17 +0800

    模板学习

--- hello/app.py
+++ hello/app.py
@@ -1,11 +1,13 @@
-from flask import Flask,redirect,url_for,jsonify,make_response,request,session
+from flask import Flask,redirect,url_for,jsonify,make_response,request,session,render_template
+from urllib.parse import urlparse,urljoin
 app = Flask(__name__)
+app.secret_key='abc'
 
 
 #内部地址跳转
 @app.route('/')
 def index():
-    return redirect(url_for('test1'))
+    return redirect(url_for('test2'))
 
 
 #flask 封装的json处理
@@ -20,7 +22,6 @@ def test2():
     response = make_response('<h1>iiii</h1>')
     response.mimetype = 'text/plain'
     return response
-
 #设置cookie（有安全漏洞）
 @app.route('/test3')
 def test3():
@@ -41,6 +42,7 @@ def login():
     session['logged_in'] = True # 写入session
     return redirect(url_for('hello'))
 
+
 @app.route('/hello')
 def hello():
     name = request.args.get('name')
@@ -59,9 +61,54 @@ def hello():
 def logout():
     if 'logged_in' in session:
         session.pop('logged_in')
-    return redirect(url_for('hello')
+    return redirect(url_for('hello'))
 
 
-114
+#request.full_path获取当前页面的相对路径，在do_someting之后重定向回去上一个页面，场景：登录之后返回之前访问的页面
+#request.referrer 获取请求来源
+@app.route('/foo')
+def foo():
+    return '<h1>Foo page</h1><a href="%s">Do something and redirect</a>' % url_for('do_something',next=request.full_path)
+@app.route('/bar')
+def bar():
+    return '<h1>Bar page</h1><a href="%s">Do something and redirect</a>' % url_for('do_something',next=request.full_path)
+@app.route('/do_something')
+def do_something():
+# do something
+    return redirect_back()
+#封装重定向函数
+def redirect_back(default='hello', **kwargs):
+    for target in request.args.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return redirect(target)
+    return redirect(url_for(default, **kwargs))
+#封装，验证重定向的地址是否安全（内部地址）
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
+
+user = {
+'username': 'Grey Li',
+'bio': 'A boy who loves movies and music.',
+}
+movies = [
+{'name': 'My Neighbor Totoro', 'year': '1988'},
+{'name': 'Three Colours trilogy', 'year': '1993'},
+{'name': 'Forrest Gump', 'year': '1994'},
+{'name': 'Perfect Blue', 'year': '1997'},
+{'name': 'The Matrix', 'year': '1999'},
+{'name': 'Memento', 'year': '2000'},
+{'name': 'The Bucket list', 'year': '2007'},
+{'name': 'Black Swan', 'year': '2010'},
+{'name': 'Gone Girl', 'year': '2014'}]
+@app.route('/watchlist')
+def watchlist():
+    return render_template('watchlist.html', user=user, movies=movies,test='&#9835;')
+
+
+167
 
